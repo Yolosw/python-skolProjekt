07b74209b835e6606f36e68c71244bc8e92284dc
Yolosw <68919114+Yolosw@users.noreply.github.com>
2023-09-13 19:54:52 +0200

    Match stats istället för if sats

--- FlerValsInputs/flerVal-Input.py
+++ FlerValsInputs/flerVal-Input.py
@@ -2,24 +2,24 @@ val = int(input("\n 1. Hej \n 2. Hejdå \n Svar: "))
 # Går att använda en string istället men då hade du inte använt en 1 i if statmentet då hade du använt "1"
 
 # Här så kollar vi om val är 1 eller 2 sedan skriver vi ut hej eller hejdå beronde på vad val är.
-if val == 1:
-    print("Hej")
-    # Skapar ett nytt val
-    val2 = int(input("1. Tyst \n 2. Prata \n: Svar"))
-    if val2 == 1:
-        print("Tyst")
-    elif val2 == 2:
-        print("Prata")
-elif val == 2:
-    # Skapar ett nytt val
-    print("Hejdå")
-    val2 = int(input("1. Tyst \n 2. Prata \n: Svar"))
-    if val2 == 1:
-        print("Tyst")
-    elif val2 == 2:
-        print("Prata")
-    
 
-# Tar upp om du skriver fel
-else:
-    print("Felaktigt val")
+
+match(val):
+    case 1:
+        print("Hej")
+        val2 = int(input("1. Tyst \n 2. Prata \n: Svar"))
+        match(val2):
+            case 1:
+                print("Tyst")
+            case 2:
+                print("Prata")
+    case 2:
+        print("hejdå")
+        val2 = int(input("1. Tyst \n 2. Prata \n: Svar"))
+        match(val2):
+            case 1:
+                print("Tyst")
+            case 2:
+                print("Prata")  
+
+    
