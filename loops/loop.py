57437149d0f5cc696a3195bf4267b5c979c838ce
Yolosw <68919114+Yolosw@users.noreply.github.com>
2023-09-13 03:03:44 +0200

    Loops

---
+++ loops/loop.py
@@ -0,0 +1,12 @@
+
+# Skapar en varible som kallas för i sedan skriver den ut den 10 gångr
+for i in range(10):
+    print("Denna kördes", i)
+
+# Skapar en variable som kallas för c sedan lägger vi på 1 varje gång den körs tills den har uppnått 10
+c = 0
+# Om c är mindre än 10 så körs den
+while c < 10:
+    # Läggs på 1 varje gång den körs.
+    c += 1
+    print("Denna kördes",c)
