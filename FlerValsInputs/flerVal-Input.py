11d453167e39cd06ef53b2a984728c51af07df8c
Yolosw <68919114+Yolosw@users.noreply.github.com>
2023-09-13 02:54:51 +0200

    FlerVal

---
+++ FlerValsInputs/flerVal-Input.py
@@ -0,0 +1,7 @@
+val = int(input("1. Hej \n 2. Hejdå"))
+# Går att använda en string istället men då hade du inte använt en 1 i if statmentet då hade du använt "1"
+
+if val == 1:
+    print("Hej")
+elif val == 2:
+    print("Hejdå")
