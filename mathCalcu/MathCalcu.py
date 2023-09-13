edd5387f55814e8cf5c901730f22b5699121b2e9
Yolosw <68919114+Yolosw@users.noreply.github.com>
2023-09-13 15:26:53 +0200

    Gjode koden bättre

--- mathCalcu/MathCalcu.py
+++ mathCalcu/MathCalcu.py
@@ -21,23 +21,23 @@ def hypokater(k1, hypo):
 
 
 val = int(input("\n 1. Cirkel Area,\n 2. Rektangel Area\n 3. Kvadrat Area \n 4.Pythogoras sats \n Svar: "))
-
+enhet = input("Enhet: ")
 # Välja mellan 4 olika val
 if val == 1:
     var1 = float(input("Radien på circkeln: "))
-    enhet = input("Enhet: ")
+  
     print(circleArea(var1), enhet)
 elif val == 2:
     var1, var2 = inputs(), inputs()
-    enhet = input("Enhet: ")
+    
     print(area(var1, var2), enhet)
 elif val == 3:
     var1 = inputs()
-    enhet = input("Enhet: ")
+    
     print(area(var1, var1), enhet)
 elif val == 4:
     val2 = int(input("1. båda kateter \n 2. Bara 1 katet: "))
-    enhet = input("Enhet: ")
+  
 
     if val2 == 1:
         var1, var2 = inputs(), inputs()
